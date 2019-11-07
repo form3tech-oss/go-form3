690e1daf284e267d98ce1267689480b46a95b06b
Iyad Assaf <iyadassaflondon@gmail.com>
2019-11-07 14:36:09 +0000

    Clone http client when retrying request with authenticated client, added subscription POST request test to verify

---
+++ tests/create_subscriptions_test.go
@@ -0,0 +1,39 @@
+package tests
+
+import (
+	"github.com/form3tech-oss/go-form3/pkg/form3"
+	"github.com/form3tech-oss/go-form3/pkg/generated/models"
+	"github.com/go-openapi/strfmt"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func TestCreateSubscriptions(t *testing.T) {
+	f3 := form3.New()
+
+	organisationId := strfmt.UUID(uuid.MustParse(os.Getenv("FORM3_ORGANISATION_ID")).String())
+	id := strfmt.UUID(uuid.New().String())
+	transport := "http"
+	callbackUri := "https://google.com/callback"
+	eventType := "created"
+	recordType := "PaymentSubmission"
+
+	req := f3.Subscriptions.CreateSubscription()
+	req.WithData(models.Subscription{
+		ID: &id,
+		OrganisationID: &organisationId,
+		Attributes: &models.SubscriptionAttributes{
+			CallbackTransport: &transport,
+			CallbackURI:       &callbackUri,
+			UserID:            strfmt.UUID(uuid.New().String()),
+			EventType: &eventType,
+			RecordType: &recordType,
+		},
+	})
+
+	resp, err := req.Do()
+	require.Nil(t, err)
+	require.Equal(t, id.String(), resp.Data.ID.String())
+}
