cda0f5f48bb6c248213e52c5cc2a0d7c83f40d6b
Robin Hallabro-Kokko <robin.hallabro-kokko@form3.tech>
2021-05-28 10:05:22 +0200

    pkg/form3/auth_token_based: properly wrap errors

--- pkg/form3/auth_token_based.go
+++ pkg/form3/auth_token_based.go
@@ -11,6 +11,7 @@ import (
 )
 
 const (
+	// #nosec G101
 	tokenBasedAuthEndpoint = "/v1/oauth2/token"
 )
 
@@ -66,11 +67,13 @@ func (t *tokenBasedTransport) RoundTrip(req *http.Request) (*http.Response, erro
 	}
 
 	var requestBodyClone io.Reader
+
 	if req.Body != nil {
 		originalRequestBody, err := ioutil.ReadAll(req.Body)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("could read request body: %w", err)
 		}
+
 		req.Body = ioutil.NopCloser(bytes.NewBuffer(originalRequestBody))
 		requestBodyClone = bytes.NewBuffer(originalRequestBody)
 	}
@@ -79,29 +82,29 @@ func (t *tokenBasedTransport) RoundTrip(req *http.Request) (*http.Response, erro
 	if res != nil && (res.StatusCode == http.StatusUnauthorized || res.StatusCode == http.StatusForbidden) {
 		authRequest, err := http.NewRequest(http.MethodPost, t.config.hostURL.String()+tokenBasedAuthEndpoint, nil)
 		if err != nil {
-			return nil, fmt.Errorf("could not build auth request, error: %v", err)
+			return nil, fmt.Errorf("could not build auth request, error: %w", err)
 		}
 		authRequest.SetBasicAuth(t.config.clientID, t.config.clientSecret)
 
 		authResponse, err := t.underlyingTransport.RoundTrip(authRequest)
 		if err != nil {
-			return nil, fmt.Errorf("error authenticating, error: %v", err)
+			return nil, fmt.Errorf("error authenticating, error: %w", err)
 		}
 		defer authResponse.Body.Close()
 
 		if authResponse.StatusCode != http.StatusOK {
-			return nil, fmt.Errorf("non 200 status code getting token, status code: %d", authResponse.StatusCode)
+			return nil, fmt.Errorf("non 200 status code getting token, got status code: %d", authResponse.StatusCode)
 		}
 
 		authBody, err := ioutil.ReadAll(authResponse.Body)
 		if err != nil {
-			return nil, fmt.Errorf("could not read auth response body, error: %v", err)
+			return nil, fmt.Errorf("could not read auth response body, error: %w", err)
 		}
 
 		var authJSON authJSONResponse
 		err = json.Unmarshal(authBody, &authJSON)
 		if err != nil {
-			return nil, fmt.Errorf("could not parse auth json response, error: %v", err)
+			return nil, fmt.Errorf("could not parse auth json response, error: %w", err)
 		}
 
 		t.token = authJSON.AccessToken
@@ -109,8 +112,9 @@ func (t *tokenBasedTransport) RoundTrip(req *http.Request) (*http.Response, erro
 
 		retryRequest, err := http.NewRequest(req.Method, req.URL.String(), requestBodyClone)
 		if err != nil {
-			return nil, fmt.Errorf("could not build authenticated request, error: %v", err)
+			return nil, fmt.Errorf("could not build authenticated request, error: %w", err)
 		}
+
 		retryRequest.Header = req.Header
 		retryRequest.Header.Add("Authorization", fmt.Sprintf("Bearer %s", t.token))
 
