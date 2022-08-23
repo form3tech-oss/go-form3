bb698431ffb2e909734b0747bd4f64cb9494bf7a
Georgian Micsa <georgian.micsa@form3.tech>
2022-08-23 13:47:03 +0300

    Add more tests for create subscriptions
    Co-authored-by: Jerzy Dudek <jerzy.dudek@form3.tech>

--- pkg/form3/subscriptions_test.go
+++ pkg/form3/subscriptions_test.go
@@ -12,28 +12,66 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateSubscriptions(t *testing.T) {
-	f3, err := form3.NewFromEnv()
-	require.NoError(t, err)
-
-	organisationID := strfmt.UUID(uuid.MustParse(os.Getenv("FORM3_ORGANISATION_ID")).String())
-	id := strfmt.UUID(uuid.New().String())
+func TestCreateSubscriptionsDeprecatedCallbackUriParams(t *testing.T) {
 	transport := models.CallbackTransportHTTP
 	callbackURI := "https://webhook.site/db28eac0-d0a5-4078-b9be-0e4617471b01"
 	eventType := "created"
 	recordType := "PaymentSubmission"
 
+	testCreateSubscriptions(t, &models.SubscriptionAttributes{
+		CallbackTransport: transport,
+		CallbackURI:       callbackURI,
+		UserID:            strfmt.UUID(uuid.New().String()),
+		EventType:         &eventType,
+		RecordType:        &recordType,
+	})
+}
+
+func TestCreateSubscriptionsNewCallbackUriParams(t *testing.T) {
+	transport := models.CallbackTransportHTTP
+	callbackURI := "https://webhook.site/db28eac0-d0a5-4078-b9be-0e4617471b01"
+	eventType := "created"
+	recordType := "PaymentSubmission"
+
+	testCreateSubscriptions(t, &models.SubscriptionAttributes{
+		CallbackUris: []*models.CallbackURI{{
+			CallbackTransport: &transport,
+			CallbackURI:       &callbackURI,
+		}},
+		UserID:     strfmt.UUID(uuid.New().String()),
+		EventType:  &eventType,
+		RecordType: &recordType,
+	})
+}
+
+func TestCreateSubscriptionsNewCallbackUriParamsHttpAwsPrivate(t *testing.T) {
+	transport := models.CallbackTransportHTTPAwsPrivate
+	callbackURI := "https://webhook.site/db28eac0-d0a5-4078-b9be-0e4617471b01"
+	eventType := "created"
+	recordType := "PaymentSubmission"
+
+	testCreateSubscriptions(t, &models.SubscriptionAttributes{
+		CallbackUris: []*models.CallbackURI{{
+			CallbackTransport: &transport,
+			CallbackURI:       &callbackURI,
+		}},
+		UserID:     strfmt.UUID(uuid.New().String()),
+		EventType:  &eventType,
+		RecordType: &recordType,
+	})
+}
+
+func testCreateSubscriptions(t *testing.T, attributes *models.SubscriptionAttributes) {
+	f3, err := form3.NewFromEnv()
+	require.NoError(t, err)
+
+	organisationID := strfmt.UUID(uuid.MustParse(os.Getenv("FORM3_ORGANISATION_ID")).String())
+	id := strfmt.UUID(uuid.New().String())
 	req := f3.Subscriptions.CreateSubscription()
 	req.WithData(models.Subscription{
 		ID:             &id,
 		OrganisationID: &organisationID,
-		Attributes: &models.SubscriptionAttributes{
-			CallbackTransport: transport,
-			CallbackURI:       callbackURI,
-			UserID:            strfmt.UUID(uuid.New().String()),
-			EventType:         &eventType,
-			RecordType:        &recordType,
-		},
+		Attributes:     attributes,
 	})
 
 	resp, err := req.Do()
