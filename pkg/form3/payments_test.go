9291a977bc19cd16895d65a76c1abdd808ce40f5
Georgian Micsa <georgian.micsa@form3.tech>
2022-08-25 13:04:06 +0300

    Bump to v5 because of breaking changes
    Co-authored-by: Jerzy Dudek <jerzy.dudek@form3.tech>

--- pkg/form3/payments_test.go
+++ pkg/form3/payments_test.go
@@ -4,7 +4,7 @@ import (
 	"io/ioutil"
 	"testing"
 
-	"github.com/form3tech-oss/go-form3/v4/pkg/form3"
+	"github.com/form3tech-oss/go-form3/v5/pkg/form3"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
--- pkg/generated/client/claims/create_claim_requests.go
+++ pkg/generated/client/claims/create_claim_requests.go
@@ -15,7 +15,7 @@ import (
 
 	strfmt "github.com/go-openapi/strfmt"
 
-	"github.com/form3tech-oss/go-form3/v4/pkg/generated/models"
+	"github.com/form3tech-oss/go-form3/v5/pkg/generated/models"
 )
 
 // Client.CreateClaim creates a new CreateClaimRequest object
